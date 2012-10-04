2930988f0f87c8d14cfc64b17c76566f81e5152c
Brian Lough <ecenuig@gmail.com>
2012-10-04 23:38:02 +0100

    Fix for server blocking parse ini

--- Common/Common.php
+++ Common/Common.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+
+PHP on server has parse ini blocked, revisit
+
 function getIrishStores()
 {
 	$irishStoresFile = "IrishStores.data";
@@ -10,6 +14,7 @@ function getIrishStores()
 	
 }
 
+
 function getNIStores()
 {
 	$niStoresFile = "NIStores.data";
@@ -19,6 +24,7 @@ function getNIStores()
 	return $niStoresFile;
 	
 }
+*/
 
 function validateProductId($productId)
 {
@@ -31,6 +37,73 @@ function validateProductId($productId)
 	//TODO Validate characters and length(7?).
 	
 	return $productId;
+}
+
+function getIrishStores()
+{
+	$stores = array(
+					"Arklow (Extra)" => 4101,
+					"Ashbourne Retail Park" => 943,
+					"Athlone" => 262,
+					"Blanchardstown Shopping Centre" => 398,
+					"Blanchardstown West End" => 669,
+					"Carlow (Extra)" => 4130,
+					"Castlebar" => 807,
+					"Cavan (Extra)" => 814,
+					"Clonmel (Extra)" => 4214,
+					"Cork Mahon (Extra)" => 4113,
+					"Cork Queens Old Castle" => 45,
+					"Cork Retail Park" => 801,
+					"Drogheda (Extra)" => 875,
+					"Dun Laoghaire" => 200,
+					"Dundalk Retail Park (Extra)" => 931,
+					"Dundrum" => 817,
+					"Galway" => 547,
+					"Ilac Centre Dublin" => 394,
+					"Jervis Street Dublin" => 397,
+					"Kilkenny" => 201,
+					"Killarney (Extra)" => 899,
+					"Letterkenny (Extra)" => 793,
+					"Liffey Valley (Extra)" => 687,
+					"Limerick Childers Road (Extra)" => 915,
+					"Limerick Cruises Street" => 393,
+					"Limerick The Crescent" => 583,
+					"Longford (Extra)" => 880,
+					"Monaghan (Extra)" => 945,
+					"Naas (Extra)" => 4218,
+					"Navan" => 832,
+					"Nutgrove" => 392,
+					"Omni Park Dublin (Extra)" => 4150,
+					"Portlaoise (Extra)" => 4125,
+					"Sligo (Extra)" => 4146,
+					"St Stephens Green Dublin" => 584,
+					"Swords (Extra)" => 581,
+					"Tallaght" => 395,
+					"Tralee (Extra)" => 11,
+					"Tullamore (Extra)" => 879,
+					"Waterford" => 396,
+					"Wexford (Extra)" => 826
+					);
+					
+	return $stores;
+}
+
+function getNIStores()
+{
+	$storesNI = array(
+						"Belfast Roses Court (Extra)" => 484,
+						"Belfast Castle Court (Extra)" => 840,
+						"Newtownabbey (Extra)" => 537,
+						"Newtownards" => 792,
+						"Bangor Flagship Centre" => 213,
+						"Belfast Royal Avenue" => 217,
+						"Belfast Connswater" => 841,
+						"Lisburn Square" => 586,
+						"Lisburn Sprucefield ((Extra))" => 835,
+						"Bangor Bloomfield Centre" => 838
+						);
+						
+	return $storesNI;
 }
 
 ?>
